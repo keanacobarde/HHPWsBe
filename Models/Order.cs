74db0a2ac401418ba03ecfc7201e8270fd014bcc
Keana Cobarde <keanacobarde@gmail.com>
2024-04-06 13:04:12 -0500

    Adding total logic

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace HHPWsBe.Models
 {
@@ -13,5 +14,22 @@ namespace HHPWsBe.Models
         public string OrderType { get; set; }
         public string PaymentType { get; set; }
         public ICollection<Item> Items { get; set; }
+
+        public decimal? Total
+        {
+            get
+            {
+                if (Items.Count() != 0)
+                {
+                    decimal total = 0;
+                    foreach (var item in Items)
+                    {
+                        total += item.Price;
+                    }
+                    return total;
+                }
+                return null;
+            }
+        }
     }
 }
