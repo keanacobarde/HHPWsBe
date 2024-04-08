9c46296ba91d4a6022ecb1ef89d0d30e59cf4940
Keana Cobarde <keanacobarde@gmail.com>
2024-04-08 18:32:32 -0500

    Adding test data for users and items

--- Data/UsersData.cs
+++ Data/UsersData.cs
@@ -5,6 +5,9 @@ namespace HHPWsBe.Data
     public class UsersData
     {
         public static List<User> Users = new List<User>()
-        { };
+        { 
+            new User() { Id = 1, IsCashier = true, Uid = "xGPegcj8fSPs1hIxDGdotN6m0fs2"},
+            new User() { Id = 2, IsCashier = true, Uid = "V5gxdnBKXlPCtnoQ3c7XsXW0pRz2"}
+        };
     }
 }
