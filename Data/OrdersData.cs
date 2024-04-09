cbb69f2c3cccaa74c599c647ce03033b0b25c44e
Keana Cobarde <keanacobarde@gmail.com>
2024-04-09 18:51:31 -0500

    Created new migration and call for adding an item to an order

--- Data/OrdersData.cs
+++ Data/OrdersData.cs
@@ -6,9 +6,9 @@ namespace HHPWsBe.Data
     {
         public static List<Order> Orders = new List<Order>
         {
-        new Order() { Id = 1, Name = "John Doe", Status = true, Phone = "123-456-7890", Email = "john@example.com", OrderType = "Dine-in", PaymentType = "Credit Card" },
-        new Order() { Id = 2, Name = "Jane Smith", Status = false, Phone = "987-654-3210", Email = "jane@example.com", OrderType = "Pickup", PaymentType = "Cash" },
-        new Order() { Id = 3, Name = "Sam Johnson", Status = true, Phone = "555-555-5555", Email = "sam@example.com", OrderType = "Delivery", PaymentType = "Online Payment" },
+        new Order() { Id = 1, Name = "John Doe", Status = true, Phone = "123-456-7890", Email = "john@example.com", OrderType = "Dine-in", PaymentType = "Credit Card", Tip = 1.25F },
+        new Order() { Id = 2, Name = "Jane Smith", Status = false, Phone = "987-654-3210", Email = "jane@example.com", OrderType = "Pickup", PaymentType = "Cash", Tip = 1.25F },
+        new Order() { Id = 3, Name = "Sam Johnson", Status = true, Phone = "555-555-5555", Email = "sam@example.com", OrderType = "Delivery", PaymentType = "Online Payment", Tip = 1.25F },
         };
     }
 }
