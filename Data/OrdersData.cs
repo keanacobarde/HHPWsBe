5a931b71b5df30031d770b90770f8869d7693c79
Keana Cobarde <keanacobarde@gmail.com>
2024-04-08 19:58:20 -0500

    Adding items to orders

--- Data/OrdersData.cs
+++ Data/OrdersData.cs
@@ -6,7 +6,7 @@ namespace HHPWsBe.Data
     {
         public static List<Order> Orders = new List<Order>()
         {
-        new Order() { Id = 1, Name = "John Doe", Status = true, Phone = "123-456-7890", Email = "john@example.com", OrderType = "Delivery", PaymentType = "Credit Card" },
+        new Order() { Id = 1, Name = "John Doe", Status = true, Phone = "123-456-7890", Email = "john@example.com", OrderType = "Dine-in", PaymentType = "Credit Card" },
         new Order() { Id = 2, Name = "Jane Smith", Status = false, Phone = "987-654-3210", Email = "jane@example.com", OrderType = "Pickup", PaymentType = "Cash" },
         new Order() { Id = 3, Name = "Sam Johnson", Status = true, Phone = "555-555-5555", Email = "sam@example.com", OrderType = "Delivery", PaymentType = "Online Payment" },
         };
