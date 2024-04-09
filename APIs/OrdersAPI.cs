290289e3c7f80a1e5718245e20b0d354e3e95e35
Keana Cobarde <keanacobarde@gmail.com>
2024-04-08 23:02:30 -0500

    Implementing Updating Orders and Adding Tip Attribute

--- APIs/OrdersAPI.cs
+++ APIs/OrdersAPI.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using HHPWsBe.Models;
+using System.Runtime.CompilerServices;
 
 namespace HHPWsBe.APIs
 {
@@ -7,6 +8,20 @@ namespace HHPWsBe.APIs
     {
         public static void Map(WebApplication app)
         {
+            app.MapPost("/orders", (HHPWsDbContext db, Order newOrder) => 
+            {
+                try
+                {
+                    db.Orders.Add(newOrder);
+                    db.SaveChanges();
+                    return Results.Created($"/api/reservations/{newOrder.Id}", newOrder);
+                }
+                catch (DbUpdateException)
+                {
+                    return Results.BadRequest("Invalid data submitted");
+                }
+            });
+            
             app.MapGet("/orders", (HHPWsDbContext db) => 
             {
                 return db.Orders.Include(o => o.Items);
@@ -20,6 +35,53 @@ namespace HHPWsBe.APIs
                     return Results.NotFound();
                 }
                 return Results.Ok(orderDetails);
+            });
+
+            app.MapPut("/orders/{id}", (HHPWsDbContext db, int id, Order updatedOrder) =>
+            {
+                Order orderToUpdate = db.Orders.FirstOrDefault(o => o.Id == id);
+                if (orderToUpdate == null)
+                {
+                    return Results.NotFound();
+                }
+
+                if (updatedOrder.Name != null)
+                { 
+                    orderToUpdate.Name = updatedOrder.Name;
+                }
+
+                if (updatedOrder.Status != null)
+                {
+                    orderToUpdate.Status = updatedOrder.Status;
+                }
+
+                if (updatedOrder.Phone != null)
+                {
+                    orderToUpdate.Phone = updatedOrder.Phone;
+                }
+
+                if (updatedOrder.Email != null)
+                {
+                    orderToUpdate.Email = updatedOrder.Email;
+                }
+
+                if (updatedOrder.OrderType != null)
+                {
+                    orderToUpdate.OrderType = updatedOrder.OrderType;
+                }
+
+                if (updatedOrder.PaymentType != null)
+                {
+                    orderToUpdate.PaymentType = updatedOrder.PaymentType;
+                }
+
+                if (updatedOrder.Tip != null)
+                {
+                    orderToUpdate.Tip = updatedOrder.Tip;
+                }
+
+                db.SaveChanges();
+                return Results.NoContent();
             });
 
             app.MapDelete("/orders/{id}", (HHPWsDbContext db, int id) =>
